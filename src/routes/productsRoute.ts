3dde6952a0db9793c67ad45c7490be5f474832e7
davizinho98 <daviseles99@gmail.com>
2022-09-14 16:11:36 -0500

    chore: adiciona middleware de erro na rota products

--- src/routes/productsRoute.ts
+++ src/routes/productsRoute.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import * as productsController from '../controllers/productsController';
+import httpErrorMiddleware from '../middlewares/error';
+import 'express-async-errors';
 
 const router = express.Router();
 
 router.get('/', productsController.getProducts);
 router.get('/:id', productsController.getProductById);
 router.post('/', productsController.createProduct);
+router.use(httpErrorMiddleware);
 
 export default router;
