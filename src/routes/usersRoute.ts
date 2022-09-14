e2c9fd11da03f1dd4f4d203a986eebe407735282
davizinho98 <daviseles99@gmail.com>
2022-09-14 16:51:52 -0500

    chore: adiciona validação de dados na rota post users

--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import * as usersController from '../controllers/usersController';
+import httpErrorMiddleware from '../middlewares/error';
+import 'express-async-errors';
 
 const router = express.Router();
 
 router.post('/', usersController.createUser);
+router.use(httpErrorMiddleware);
 
 export default router;
--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -1,9 +1,63 @@
 import * as usersModel from '../models/usersModel';
 import IUser from '../interfaces/IUser';
+import HttpException from '../shared/httpException';
+
+const validateUsername = (name: string) => {
+  if (!name) {
+    throw new HttpException(400, '"username" is required');
+  }
+  if (typeof name !== 'string') {
+    throw new HttpException(422, '"username" must be a string');
+  }
+  if (name.length < 3) {
+    throw new HttpException(422, '"username" length must be at least 3 characters long');
+  }
+};
+
+const validateClasse = (classe: string): void => {
+  if (!classe) {
+    throw new HttpException(400, '"classe" is required');
+  }
+  if (typeof classe !== 'string') {
+    throw new HttpException(422, '"classe" must be a string');
+  }
+  if (classe.length < 3) {
+    throw new HttpException(422, '"classe" length must be at least 3 characters long');
+  }
+};
+
+const validateLevel = (level: number): void => {
+  if (!level && level !== 0) {
+    throw new HttpException(400, '"level" is required');
+  }
+  if (typeof level !== 'number') {
+    throw new HttpException(422, '"level" must be a number');
+  }
+  if (level < 1) {
+    throw new HttpException(422, '"level" must be greater than or equal to 1');
+  }
+};
+
+const validatePassword = (password: string): void => {
+  if (!password) {
+    throw new HttpException(400, '"password" is required');
+  }
+  if (typeof password !== 'string') {
+    throw new HttpException(422, '"password" must be a string');
+  }
+  if (password.length < 8) {
+    throw new HttpException(422, '"password" length must be at least 8 characters long');
+  }
+};
 
 export const createUser = async (
   { username, classe, level, password }: IUser,
 ): Promise<string> => {
+  validateUsername(username);
+  validateClasse(classe);
+  validateLevel(level);
+  validatePassword(password);
+
   const token = await usersModel.createUser({ username, classe, level, password });
 
   return token;
