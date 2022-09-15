2df30b9cc661d486de6fa5e17cb7bc47322db21e
davizinho98 <daviseles99@gmail.com>
2022-09-15 18:45:11 -0500

    feat: adiciona model para buscar user pelo username

--- src/models/usersModel.ts
+++ src/models/usersModel.ts
@@ -1,7 +1,14 @@
-import { ResultSetHeader } from 'mysql2';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
 import IUser from '../interfaces/IUser';
 import getToken from '../utils/getToken';
 import connection from './connection';
+
+export const getUserByUsername = async (username: string): Promise<IUser> => {
+  const [result] = await connection
+    .execute<RowDataPacket[]>('SELECT * FROM Trybesmith.Users WHERE username = ?', [username]);
+    
+  return result[0] as IUser;
+};
 
 export const createUser = async (user: IUser): Promise<string> => {
   const { username, classe, level, password } = user;
