afa869f636252a4b8ab142a80950031218a4e39b
davizinho98 <daviseles99@gmail.com>
2022-11-07 09:55:39 -0500

    chore: seta variáveis padrão para conectar com o banco de dados do container

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -4,9 +4,10 @@ import mysql from 'mysql2/promise';
 dotenv.config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  database: process.env.MYSQL_DATABASE || 'Trybesmith',
 });
 
 export default connection;
