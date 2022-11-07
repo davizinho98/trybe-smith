4836832df4ce808e4b8dc1a0a80778f412189dca
davizinho98 <daviseles99@gmail.com>
2022-11-07 10:09:09 -0500

    fix: db

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -7,7 +7,6 @@ const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
-  database: process.env.MYSQL_DATABASE || 'Trybesmith',
 });
 
 export default connection;
